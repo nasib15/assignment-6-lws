9dbd3f84afe4e6f4bc0705931d10ce7c35e7002f
nasib15 <nasibhossain57@gmail.com>
2024-11-28 19:01:41 +0600

    Add: Popular Categories section

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import DeliciousSection from "@/components/DeliciousSection";
 import Hero from "@/components/Hero";
+import PopularCategories from "@/components/PopularCategories";
 
 export default function Home() {
   return (
     <main className="container mx-auto px-4 mt-[100px]">
       <Hero />
       <DeliciousSection />
+      <PopularCategories />
     </main>
   );
 }
