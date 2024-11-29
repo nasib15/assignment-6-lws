ae8b3ecc576083903e248ceef7ac982c25ee45dc
nasib15 <nasibhossain57@gmail.com>
2024-11-29 20:12:34 +0600

    Add: Category page dynamic

---
+++ app/categories/page.js
@@ -0,0 +1,31 @@
+import getCategories from "@/utils/getCategories";
+import { getThumbnailImage } from "@/utils/getThumbnailImage";
+import Image from "next/image";
+
+const CategoriesPage = () => {
+  const categories = getCategories();
+  return (
+    <main className="container mx-auto px-4 py-8 mt-[100px]">
+      <h1 class="text-5xl font-bold mb-12">Categories</h1>
+
+      {/* category list */}
+      <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-8">
+        {categories?.map((category) => (
+          <div key={category.id} class="text-center">
+            <div class="overflow-hidden rounded-full mb-4 relative cursor-pointer">
+              <Image
+                src={getThumbnailImage(category?.image)}
+                alt={category.title}
+                width={181}
+                height={181}
+                class="w-full h-auto transform transition-transform duration-300 ease-in-out hover:scale-110"
+              />
+            </div>
+            <h2 class="text-xl font-semibold">{category?.name}</h2>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+};
+export default CategoriesPage;
